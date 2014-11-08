9bd49e51c8595ee230d1686a83236fcdf6cecebc
fmartins89 <fmartins89@gmail.com>
2014-11-08 01:08:38 +0000

    template para cadastro de usuarios

--- sites/all/themes/bootstrap/bootstrap_subtheme/templates/user-register-form.tpl.php
+++ sites/all/themes/bootstrap/bootstrap_subtheme/templates/user-register-form.tpl.php
@@ -1,7 +1,58 @@
+<div class="row">
+    <div class="col-md-4">
+    </div>
+    <div class="col-md-4">
+        <?php print render($form['field_image']);?>
+    </div>
+
+</div>
+
+<div class="row">
+    <div class="col-md-2">
+    </div>
+    <div class="col-md-8">
+        <?php print render($form['field_nome']);?>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-md-2">
+    </div>
+    <div class="col-md-4">
+        <?php print render($form['account']['mail']);?>
+    </div>
+    <div class="col-md-4">
+        <?php print render($form['field_cpf']);?>
+    </div>
+
+</div>
+
+<div class="row">
+    <div class="col-md-4">
+    </div>
+    <div class="col-md-4">
+        <?php print render($form['field_data_de_nascimento']);?>
+    </div>
+</div>
+<div class="row">
+    <div class="col-md-4">
+    </div>
+    <div class="col-md-2">
+        <?php print render($form['field_estado_civil']);?>
+    </div>
+    <div class="col-md-2">
+        <?php print render($form['field_sexo']);?>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-md-5">
+    </div>
+    <div class="col-md-2">
+        <?php print render($form['actions']);?>
+    </div>
+</div>
 <div>
-    <?php print render($form['actions'])?>
-<div>
-<div>
-    <?php dpm($variables); ?>
+    <?php dpm($variables['form']); ?>
     <?php //print drupal_render_children($form); ?>
-<div>
+</div>
