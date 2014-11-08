9c0f04001feba0791c6ce79d12a853acb9cbf389
Matheus Svolenski <msvolenski@gmail.com>
2014-11-08 01:21:05 +0000

    Define what to show or not in the Caronas form

--- sites/all/themes/bootstrap/bootstrap_subtheme/templates/carona-node-form.tpl.php
+++ sites/all/themes/bootstrap/bootstrap_subtheme/templates/carona-node-form.tpl.php
@@ -1,3 +1,25 @@
 <div>
-<?php print 123;//dpm($variables); ?>
+    <div class="row">
+        <div class="col-md-6">
+            <?php hide($form['field_origem']['und']['0']['locpick']['user_latitude']); ?>
+            <?php hide($form['field_origem']['und']['0']['locpick']['user_longitude']); ?>
+            <?php hide($form['field_origem']['und']['0']['locpick']['current']); ?>
+            <?php hide($form['field_origem']['und']['0']['locpick']['map_instructions']); ?>
+            <?php hide($form['field_origem']['und']['0']['country']); ?>
+            <?php print render($form['field_origem']); ?>
+        </div>
+        <div class="col-md-6">
+            <?php hide($form['field_destino']['und']['0']['locpick']['user_latitude']); ?>
+            <?php hide($form['field_destino']['und']['0']['locpick']['user_longitude']); ?>
+            <?php hide($form['field_destino']['und']['0']['locpick']['current']); ?>
+            <?php hide($form['field_destino']['und']['0']['locpick']['map_instructions']); ?>
+            <?php hide($form['field_destino']['und']['0']['country']); ?>
+            <?php print render($form['field_destino']); ?>
+        </div>
+    </div>
+    
+<?php print render($form['additional_settings']['group']); ?>
+<?php print drupal_render_children($form); ?>
+
+<?php dpm($variables); ?>
 </div>
