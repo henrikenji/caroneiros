b3c6f2a301bddc2ccacdd18831943b2f77c016bb
Matheus Svolenski <msvolenski@gmail.com>
2014-12-05 23:21:22 -0200

    Organiza página de login, cadastro e recuperação de senha

---
+++ sites/all/themes/bootstrap/bootstrap_subtheme/templates/user-pass.tpl.php
@@ -0,0 +1,16 @@
+<div class="row">
+    <div class="col-md-4 col-md-offset-4">
+        <?php print render($form['name']);?>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-md-2 col-md-offset-4">
+        <?php print render($form['actions']);?>
+    </div>
+</div>
+
+<div>
+    <?php //dpm($variables['form']); ?>
+    <?php print drupal_render_children($form); ?>
+</div>
--- sites/all/themes/bootstrap/bootstrap_subtheme/templates/user-register-form.tpl.php
+++ sites/all/themes/bootstrap/bootstrap_subtheme/templates/user-register-form.tpl.php
@@ -1,58 +1,57 @@
 <div class="row">
-    <div class="col-md-4">
+    <div class="col-md-8 col-md-offset-2">
+        <?php print render($form['account']['name']);?>
     </div>
-    <div class="col-md-4">
-        <?php print render($form['field_image']);?>
-    </div>
-
 </div>
 
 <div class="row">
-    <div class="col-md-2">
-    </div>
-    <div class="col-md-8">
+    <div class="col-md-8 col-md-offset-2">
         <?php print render($form['field_nome']);?>
     </div>
 </div>
 
 <div class="row">
-    <div class="col-md-2">
-    </div>
-    <div class="col-md-4">
+    <div class="col-md-4 col-md-offset-2">
         <?php print render($form['account']['mail']);?>
     </div>
     <div class="col-md-4">
         <?php print render($form['field_cpf']);?>
     </div>
-
 </div>
 
 <div class="row">
-    <div class="col-md-4">
+    <div class="col-md-8 col-md-offset-2">
+        <?php print render($form['account']['pass_field']);?>
     </div>
-    <div class="col-md-4">
+</div>
+
+<div class="row">
+    <div class="col-md-8 col-md-offset-2">
+        <?php print render($form['picture']);?>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-md-4 col-md-offset-2">
         <?php print render($form['field_data_de_nascimento']);?>
     </div>
-</div>
-<div class="row">
-    <div class="col-md-4">
-    </div>
-    <div class="col-md-2">
-        <?php print render($form['field_estado_civil']);?>
-    </div>
-    <div class="col-md-2">
-        <?php print render($form['field_sexo']);?>
-    </div>
+	<div class="col-md-4">
+		<div class="col-md-5">
+			<?php print render($form['field_estado_civil']);?>
+		</div>
+		<div class="col-md-5">
+			<?php print render($form['field_sexo']);?>
+		</div>
+	</div>
 </div>
 
 <div class="row">
-    <div class="col-md-5">
-    </div>
-    <div class="col-md-2">
+    <div class="col-md-2 col-md-offset-5">
         <?php print render($form['actions']);?>
     </div>
 </div>
+
 <div>
-    <?php dpm($variables['form']); ?>
-    <?php //print drupal_render_children($form); ?>
+    <?php //dpm($variables['form']); ?>
+    <?php print drupal_render_children($form); ?>
 </div>
