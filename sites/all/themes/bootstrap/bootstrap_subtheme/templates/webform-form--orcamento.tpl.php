37caf514964b962fa70262838c850d439f73e95c
fmartins89 <fmartins89@gmail.com>
2014-12-13 01:07:05 +0000

    adicionado template para a pagina orcamento

--- sites/all/themes/bootstrap/bootstrap_subtheme/templates/webform-form--orcamento.tpl.php
+++ sites/all/themes/bootstrap/bootstrap_subtheme/templates/webform-form--orcamento.tpl.php
@@ -1,3 +1,29 @@
+
+
+<div class="row">
+    <div class="col-md-8 col-md-offset-2">
+        <?php print render($form['submitted']['nome_da_empresa']);?>
+    </div>
+</div>
+<div class="row">
+    <div class="col-md-8 col-md-offset-2">
+        <?php print render($form['submitted']['responsavel']);?>
+    </div>
+</div>
+<div class="row">
+    <div class="col-md-4 col-md-offset-2">
+        <?php print render($form['submitted']['e_mail']);?>
+    </div>
+    <div class="col-md-4">
+        <?php print render($form['submitted']['telefone']);?>
+    </div>
+</div>
+<div class="row">
+    <div class="text-center">
+        <?php print render($form['actions']);?>
+    </div>
+</div>
+
 <div>
     <?php dpm($form)?>
     <?php print drupal_render_children($form);?>
