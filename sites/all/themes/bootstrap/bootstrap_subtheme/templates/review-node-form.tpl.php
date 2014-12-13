0fdbd04324816e481b902e1882e56d2efbc4158e
martinsrenato <martinsrenato@gmail.com>
2014-12-13 00:50:59 +0000

    organizando pagina de reviews

---
+++ sites/all/themes/bootstrap/bootstrap_subtheme/templates/review-node-form.tpl.php
@@ -0,0 +1,26 @@
+<div class="row">
+    <div class="col-md-8 col-md-offset-2">
+        <?php print render($form['field_user']);?>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-md-6 col-md-offset-2">
+        <?php print render($form['title']);?>
+    </div>
+    <div class="col-md-2">
+        <span class="pull-right"><?php print render($form['field_rating']);?></span>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-md-8 col-md-offset-2">
+        <?php print render($form['body']);?>
+    </div>
+</div>
+
+<div class="row">
+    <div class="text-center">
+        <?php print render($form['actions']);?>
+    </div>
+</div>
