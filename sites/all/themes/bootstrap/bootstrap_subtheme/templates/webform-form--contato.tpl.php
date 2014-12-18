7f941ba4a4809a497beeb759c9f3d092c06a0b7d
fmartins89 <fmartins89@gmail.com>
2014-12-18 01:07:03 +0000

    formulario fale conosco reposicionado

--- sites/all/themes/bootstrap/bootstrap_subtheme/templates/webform-form--contato.tpl.php
+++ sites/all/themes/bootstrap/bootstrap_subtheme/templates/webform-form--contato.tpl.php
@@ -1,31 +1,40 @@
 
 <div class="row">
-    <div class="col-md-8 col-md-offset-2">
+    <div class="col-md-4 col-md-offset-2">
         <?php print render($form['submitted']['nome']);?>
     </div>
-</div>
-<div class="row">
-    <div class="col-md-8 col-md-offset-2">
+    <div class="col-md-4" >
         <?php print render($form['submitted']['e_mail']);?>
     </div>
 </div>
 <div class="row">
-    <div class="col-md-4 col-md-offset-2">
+
+</div>
+
+<div class="row"><br></div>
+
+<div class="row">
+    <div class="col-md-8 col-md-offset-2">
         <?php print render($form['submitted']['assunto']);?>
     </div>
 </div>
+
+
 <div class="row">
     <div class="col-md-8 col-md-offset-2">
         <?php print render($form['submitted']['mensagem']);?>
     </div>
 </div>    
+
+<div class="row"><br></div>
+
 <div class="row">
     <div class="text-center">
         <?php print render($form['actions']);?>
     </div>
 </div>
 
-<?php dpm($form);?>
+<?php //dpm($form);?>
 <div>
     <?php print drupal_render_children($form);?>
 </div>
