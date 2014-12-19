69ff939b6af87d35739a1333a9eef47b09333212
Matheus Svolenski <msvolenski@gmail.com>
2014-12-18 23:00:30 -0200

    Correções nas páginas de Fale Conosco e Orçamentos e estilo da página de Nova Carona

--- sites/all/themes/bootstrap/bootstrap_subtheme/templates/carona-node-form.tpl.php
+++ sites/all/themes/bootstrap/bootstrap_subtheme/templates/carona-node-form.tpl.php
@@ -1,11 +1,24 @@
-<div>
+<div class="col-md-10 col-md-offset-1">
     <?php print render($form['title']); ?>
     <?php hide($form['body']['und']['0']['summary']); ?>
     <?php hide($form['body']['und']['0']['format']); ?>
     <?php print render($form['body']); ?>
-    <?php print render($form['field_vagas']); ?>
-    <?php print render($form['field_data']); ?>
-    <?php print render($form['field_tipo']); ?>
+	<div class="row">
+		<div class="col-md-5" style="padding-left:30px">
+			<div class="row">
+				<?php print render($form['field_vagas']); ?>
+			</div>
+			
+			<div class="row">
+				<?php print render($form['field_tipo']); ?>
+			</div>
+		</div>
+		
+		<div class="col-md-7">
+			<?php print render($form['field_data']); ?>
+		</div>
+	</div>
+	
     <div class="row">
         <div class="col-md-6">
             <?php //hide($form['field_origem']['und']['0']['locpick']['user_latitude']); ?>
@@ -16,7 +29,7 @@
             <?php hide($form['field_origem']['und']['0']['country']); ?>
             <?php hide($form['field_origem']['und']['0']['delete_location']); ?>
             <?php //hide($form['field_origem']['und']['0']['province']['#autocomplete_path']); ?>
-            <?php print render($form['field_origem']); ?>
+			<?php print render($form['field_origem']); ?>
         </div>
         <div class="col-md-6">
             <?php //hide($form['field_destino']['und']['0']['locpick']['user_latitude']); ?>
@@ -32,7 +45,9 @@
     </div>
     
     <?php hide($form['additional_settings']['group']); ?>
-    <?php print render($form['actions']); ?>
+    <div class="text-center">
+		<?php print render($form['actions']); ?>
+	</div>
     <?php print drupal_render_children($form); ?>
 
 <?php //dpm($variables); ?>
