aa56e80170e3ff63422d8e086af247d76d7db349
Henrique Kenji <henrique.kenji.umezaki@gmail.com>
2014-12-18 23:01:33 -0200

    tabelas no perfil de usuario

--- sites/all/themes/bootstrap/bootstrap_subtheme/templates/user-profile.tpl.php
+++ sites/all/themes/bootstrap/bootstrap_subtheme/templates/user-profile.tpl.php
@@ -1,8 +1,8 @@
 <div class="profile"<?php print $attributes; ?>>
     <div class="row">
         <div class="col-md-6">
-            <?php print render($user_picture); ?>  
-        </div>  
+            <?php print render($user_picture); ?>
+        </div>
         <div class="col-md-6">
             <?php print render($field_nome); ?>
             <?php print render($field_data_de_nascimento); ?>
@@ -16,4 +16,23 @@
             <?php print render($review); ?>
         </div>
     <?php endif;?>
+    <?php if (!empty($historico)): ?>
+				<h3><?php print $title; ?></h3>
+        <div class="historico">
+            <?php print $historico; ?>
+        </div>
+    <?php endif;?>
+    <?php if (!empty($caronas)): ?>
+				<h3><?php print $caronas_title; ?></h3>
+        <div class="caronas">
+            <?php print $caronas; ?>
+        </div>
+    <?php endif;?>
+    <?php if (!empty($solicitacao)): ?>
+      <h3><?php print $solicitacao_title; ?></h3>
+      <div class="solicitacao">
+        <?php print $solicitacao; ?>
+      </div>
+    <?php endif;?>
+
 </div>
